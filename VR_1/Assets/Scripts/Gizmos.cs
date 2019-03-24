60deb4f55be9bbb5421552074297ee29b7c8b9c3
tabuadaefren <tabuadaefren>
2019-03-23 21:53:55 -0700

    initial commit

---
+++ VR_1/Assets/Scripts/Gizmos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+
+public class Gizmos : MonoBehaviour
+{
+    private static Color color;
+    public float gizmoSize = 0.75f;
+    public Color gizmoColor = Color.blue;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+
+    private static void DrawWireSphere(Vector3 position, float gizmoSize)
+    {
+        throw new NotImplementedException();
+    }
+}
